08d237ab29e82d9d9c325852369067b91f4e3554
Giorgos Komninos <admin@gkomninos.com>
2024-10-12 14:56:04 +0300

    Refactors main.go to user the Runner interface

---
+++ runner/databaserunner/databaserunner.go
@@ -0,0 +1,153 @@
+package databaserunner
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"io"
+	"os"
+
+	// postgres driver
+	_ "github.com/jackc/pgx/v5/stdlib"
+
+	"github.com/gosom/google-maps-scraper/postgres"
+	"github.com/gosom/google-maps-scraper/runner"
+	"github.com/gosom/scrapemate"
+	"github.com/gosom/scrapemate/scrapemateapp"
+)
+
+type dbrunner struct {
+	cfg      *runner.Config
+	provider scrapemate.JobProvider
+	produce  bool
+	app      *scrapemateapp.ScrapemateApp
+	conn     *sql.DB
+}
+
+func New(cfg *runner.Config) (runner.Runner, error) {
+	if cfg.RunMode != runner.RunModeDatabase && cfg.RunMode != runner.RunModeDatabaseProduce {
+		return nil, fmt.Errorf("%w: %d", runner.ErrInvalidRunMode, cfg.RunMode)
+	}
+
+	conn, err := openPsqlConn(cfg.Dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ans := dbrunner{
+		cfg:      cfg,
+		provider: postgres.NewProvider(conn),
+		produce:  cfg.ProduceOnly,
+		conn:     conn,
+	}
+
+	if ans.produce {
+		return &ans, nil
+	}
+
+	psqlWriter := postgres.NewResultWriter(conn)
+
+	writers := []scrapemate.ResultWriter{
+		psqlWriter,
+	}
+
+	opts := []func(*scrapemateapp.Config) error{
+		// scrapemateapp.WithCache("leveldb", "cache"),
+		scrapemateapp.WithConcurrency(cfg.Concurrency),
+		scrapemateapp.WithProvider(ans.provider),
+		scrapemateapp.WithExitOnInactivity(cfg.ExitOnInactivityDuration),
+	}
+
+	if cfg.Debug {
+		opts = append(opts, scrapemateapp.WithJS(
+			scrapemateapp.Headfull(),
+			scrapemateapp.DisableImages(),
+		))
+	} else {
+		opts = append(opts, scrapemateapp.WithJS(scrapemateapp.DisableImages()))
+	}
+
+	matecfg, err := scrapemateapp.NewConfig(
+		writers,
+		opts...,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	ans.app, err = scrapemateapp.NewScrapeMateApp(matecfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ans, nil
+}
+
+func (d *dbrunner) Run(ctx context.Context) error {
+	if d.produce {
+		return d.produceSeedJobs(ctx)
+	}
+
+	return d.app.Start(ctx)
+}
+
+func (d *dbrunner) Close(context.Context) error {
+	if d.app != nil {
+		return d.app.Close()
+	}
+
+	if d.conn != nil {
+		return d.conn.Close()
+	}
+
+	return nil
+}
+
+func (d *dbrunner) produceSeedJobs(ctx context.Context) error {
+	var input io.Reader
+
+	switch d.cfg.InputFile {
+	case "stdin":
+		input = os.Stdin
+	default:
+		f, err := os.Open(d.cfg.InputFile)
+		if err != nil {
+			return err
+		}
+
+		defer f.Close()
+
+		input = f
+	}
+
+	jobs, err := runner.CreateSeedJobs(
+		d.cfg.LangCode,
+		input,
+		d.cfg.MaxDepth,
+		d.cfg.Email,
+		d.cfg.GeoCoordinates,
+		d.cfg.Zoom,
+	)
+	if err != nil {
+		return err
+	}
+
+	for i := range jobs {
+		if err := d.provider.Push(ctx, jobs[i]); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func openPsqlConn(dsn string) (conn *sql.DB, err error) {
+	conn, err = sql.Open("pgx", dsn)
+	if err != nil {
+		return
+	}
+
+	err = conn.Ping()
+
+	return
+}
---
+++ runner/jobs.go
@@ -0,0 +1,74 @@
+package runner
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"plugin"
+	"strings"
+
+	"github.com/gosom/google-maps-scraper/gmaps"
+	"github.com/gosom/scrapemate"
+)
+
+func CreateSeedJobs(langCode string, r io.Reader, maxDepth int, email bool, geoCoordinates string, zoom int) (jobs []scrapemate.IJob, err error) {
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		query := strings.TrimSpace(scanner.Text())
+		if query == "" {
+			continue
+		}
+
+		var id string
+
+		if before, after, ok := strings.Cut(query, "#!#"); ok {
+			query = strings.TrimSpace(before)
+			id = strings.TrimSpace(after)
+		}
+
+		jobs = append(jobs, gmaps.NewGmapJob(id, langCode, query, maxDepth, email, geoCoordinates, zoom))
+	}
+
+	return jobs, scanner.Err()
+}
+
+func LoadCustomWriter(pluginDir, pluginName string) (scrapemate.ResultWriter, error) {
+	files, err := os.ReadDir(pluginDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read plugin directory: %w", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		if filepath.Ext(file.Name()) != ".so" && filepath.Ext(file.Name()) != ".dll" {
+			continue
+		}
+
+		pluginPath := filepath.Join(pluginDir, file.Name())
+
+		p, err := plugin.Open(pluginPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to open plugin %s: %w", file.Name(), err)
+		}
+
+		symWriter, err := p.Lookup(pluginName)
+		if err != nil {
+			return nil, fmt.Errorf("failed to lookup symbol %s: %w", pluginName, err)
+		}
+
+		writer, ok := symWriter.(*scrapemate.ResultWriter)
+		if !ok {
+			return nil, fmt.Errorf("unexpected type %T from writer symbol in plugin %s", symWriter, file.Name())
+		}
+
+		return *writer, nil
+	}
+
+	return nil, fmt.Errorf("no plugin found in %s", pluginDir)
+}
