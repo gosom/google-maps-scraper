16bed9e6283256447f32574fca3f8b35b3fb8eac
Georgios Komninos <454718+gosom@users.noreply.github.com>
2024-10-13 19:08:46 +0300

    More efficient database operations (#78)

--- postgres/resultwriter.go
+++ postgres/resultwriter.go
@@ -5,6 +5,9 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"strings"
+	"time"
 
 	"github.com/gosom/scrapemate"
 
@@ -20,6 +23,11 @@ type resultWriter struct {
 }
 
 func (r *resultWriter) Run(ctx context.Context, in <-chan scrapemate.Result) error {
+	const maxBatchSize = 50
+
+	buff := make([]*gmaps.Entry, 0, 50)
+	lastSave := time.Now().UTC()
+
 	for result := range in {
 		entry, ok := result.Data.(*gmaps.Entry)
 
@@ -27,7 +35,21 @@ func (r *resultWriter) Run(ctx context.Context, in <-chan scrapemate.Result) err
 			return errors.New("invalid data type")
 		}
 
-		if err := r.saveEntry(ctx, entry); err != nil {
+		buff = append(buff, entry)
+
+		if len(buff) >= maxBatchSize || time.Now().UTC().Sub(lastSave) >= time.Minute {
+			err := r.batchSave(ctx, buff)
+			if err != nil {
+				return err
+			}
+
+			buff = buff[:0]
+		}
+	}
+
+	if len(buff) > 0 {
+		err := r.batchSave(ctx, buff)
+		if err != nil {
 			return err
 		}
 	}
@@ -35,19 +57,46 @@ func (r *resultWriter) Run(ctx context.Context, in <-chan scrapemate.Result) err
 	return nil
 }
 
-func (r *resultWriter) saveEntry(ctx context.Context, entry *gmaps.Entry) error {
+func (r *resultWriter) batchSave(ctx context.Context, entries []*gmaps.Entry) error {
+	if len(entries) == 0 {
+		return nil
+	}
+
 	q := `INSERT INTO results
 		(data)
 		VALUES
-		($1) ON CONFLICT DO NOTHING
 		`
+	elements := make([]string, 0, len(entries))
+	args := make([]interface{}, 0, len(entries))
 
-	data, err := json.Marshal(entry)
+	for i, entry := range entries {
+		data, err := json.Marshal(entry)
+		if err != nil {
+			return err
+		}
+
+		elements = append(elements, fmt.Sprintf("($%d)", i+1))
+		args = append(args, data)
+	}
+
+	q += strings.Join(elements, ", ")
+	q += " ON CONFLICT DO NOTHING"
+
+	tx, err := r.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
 
-	_, err = r.db.ExecContext(ctx, q, data)
+	defer func() {
+		_ = tx.Rollback()
+	}()
+
+	_, err = tx.ExecContext(ctx, q, args...)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
 
 	return err
 }
