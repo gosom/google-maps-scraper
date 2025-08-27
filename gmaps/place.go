a64767c1b63a50a81646fa7966e9245ecbca574e
tianwang <tianwang@buaa.edu.cn>
2025-08-27 11:10:51 +0800

    优化 place.go 中的解析逻辑

--- gmaps/place.go
+++ gmaps/place.go
@@ -224,14 +224,11 @@ function parse() {
 	if (!appState) {
 		return null;
 	}
-
-	for (let i = 65; i <= 90; i++) {
-		const key = String.fromCharCode(i) + "f";
-		if (appState[key] && appState[key][6]) {
+	const keys = Object.keys(appState);
+	const key = keys[0];
+	if (appState[key] && appState[key][6]) {
 		return appState[key][6];
-		}
 	}
-
 	return null;
 }
 `
